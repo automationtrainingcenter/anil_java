579f0df6c81dde2e76a50ceb8175a4b9ca80335f
automationtrainingcenter <atcsurya@gmail.com>
2019-12-09 09:51:32 +0530

    conditional statements practicles

--- src/basics/ConditionalStatementsDemo.java
+++ src/basics/ConditionalStatementsDemo.java
@@ -1,4 +1,7 @@
 package basics;
+
+import java.util.Scanner;
+
 /*These are used to execute a block of statements based on some condition or expression
  * ******** if ************
  * if(expression){
@@ -73,5 +76,98 @@ package basics;
  */
 
 public class ConditionalStatementsDemo {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		// verify given number is positive
+		int number = -10;
+		// simple if statement
+		if (number >= 0) {
+			System.out.println("given number is positive");
+		}
+//		System.out.println("simple if completed");
+
+		// if else statement
+		// verify given number is odd or even
+		if (number % 2 == 0) {
+			System.out.println("number is even");
+		} else {
+			System.out.println("number is odd");
+		}
+
+		// facebook scenario
+		// read username
+//		System.out.println("enter username");
+//		String username = sc.next();
+//		
+//		// read password
+//		System.out.println("enter password");
+//		String password = sc.next();
+//		// compare username and passwords are valid or not
+//		if(username.equalsIgnoreCase("sunshine") && password.equals("selenium")) {
+//			System.out.println("welcome "+username.toLowerCase());
+//		}else {
+//			System.out.println("invalid credentials");
+//		}
+
+		// nested if
+		// gmail scenario
+		// read username
+//		System.out.println("enter gmail username");
+//		String guser = sc.next();
+//		// compare user name is valid or not
+//		if(guser.equalsIgnoreCase("sunshine")) {
+//			// if user name is valid then read password
+//			System.out.println("enter gmail password");
+//			String gpass = sc.next();
+//			// now compare password is valid or not
+//			if(gpass.equals("selenium")) {
+//				// if password is valid then greet the user
+//				System.out.println("Welcome "+guser.toLowerCase());
+//			}else {
+//				// if password is invalid then display invalid password
+//				System.out.println("invalid password");
+//			}
+//		}else {
+//			// if user name is invalid then display invalid user name
+//			System.out.println("invlaid username");
+//		}
+
+		// else if ladder
+		// launch a desired browser
+		System.out.println("enter browser you want to open");
+		String brName = sc.next();
+		if (brName.equalsIgnoreCase("chrome")) {
+			System.out.println("launching Chrome browser");
+		} else if (brName.equalsIgnoreCase("firefox")) {
+			System.out.println("launching firefox browser");
+		} else if (brName.equalsIgnoreCase("opera")) {
+			System.out.println("launching Opera browser");
+		} else if (brName.equalsIgnoreCase("edge")) {
+			System.out.println("launching microsoft edge browser");
+		} else {
+			System.out.println("launching Internet explorer browser");
+		}
+		System.out.println("***************************************");
+
+		// switch case
+		switch (brName.toLowerCase()) {
+		case "chrome":
+			System.out.println("launching Chrome browser");
+			break;
+		case "firefox":
+			System.out.println("launching firefox browser");
+			break;
+		case "opera":
+			System.out.println("launching opera browser");
+			break;
+		case "edge":
+			System.out.println("launching edge browser");
+			break;
+		default:
+			System.out.println("launching Internet explorer browser");
+		}
+	}
 
 }
