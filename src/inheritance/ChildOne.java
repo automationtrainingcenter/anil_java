c488118765c1f0279df0148c0704df1e0cfd1142
automationtrainingcenter <atcsurya@gmail.com>
2019-12-19 10:01:04 +0530

    inheritance demo

---
+++ src/inheritance/ChildOne.java
@@ -0,0 +1,14 @@
+package inheritance;
+
+public class ChildOne extends Parent{
+	
+	public ChildOne() {
+		super();
+		System.out.println("child class object created");
+	}
+	
+	public void methodTwo() {
+		System.out.println("child one class method two");
+	}
+
+}
